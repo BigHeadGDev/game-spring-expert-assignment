ca09d73d06fba4a1b94187de521848cb4454ec25
BigHeadGDev <goalkeeper0099@gmail.com>
2026-09-22 15:24:53 +0900

    Lv15 필수과제 끝

--- src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
+++ src/main/java/com/gameexpert/ws/handler/OnlineUsersWsHandler.java
@@ -1,5 +1,6 @@
 package com.gameexpert.ws.handler;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.gameexpert.ws.NicknameHandshakeInterceptor;
 import com.gameexpert.ws.WorldBroadcaster;
@@ -24,5 +25,30 @@ public class OnlineUsersWsHandler implements WsMessageHandler {
     @Override
     public void handle(WsMessageContext context, JsonNode message) {
         // TODO Lv 15: 현재 월드의 열린 연결에서 닉네임을 조회하고 요청자에게 응답합니다.
+        List<String> users = new ArrayList<>();
+
+        for (WorldSessionRegistry.Entry entry
+                : registry.entries(context.worldId())) {
+
+            var session = entry.session();
+
+            if (!session.isOpen()) {
+                continue;
+            }
+
+            String nickname = (String) session.getAttributes()
+                    .get(NicknameHandshakeInterceptor.ATTR_NICKNAME);
+
+            users.add(nickname);
+        }
+
+        users.sort(String::compareTo);
+
+        int count = users.size();
+
+        OnlineUsersResponse response =
+                new OnlineUsersResponse(users, count);
+
+        broadcaster.sendTo(context.session(), response);
     }
 }
